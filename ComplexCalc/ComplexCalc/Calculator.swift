46aa963016b4fa8631428a59fe05a7a6ab42d092
Benny Nguyen <benny.nguyenx@gmail.com>
2018-01-23 00:03:08 -0800

    Add subtract and add functions that take tuples/points as arguments

--- ComplexCalc/ComplexCalc/Calculator.swift
+++ ComplexCalc/ComplexCalc/Calculator.swift
@@ -14,6 +14,10 @@ class Calculator {
     func mathOp(lhs: Int, rhs: Int, op: (Int, Int) -> Int) -> Int {
         return op(lhs, rhs)
     }
+    
+    func mathOp(args: [Int], beg: Int, op: ([Int]) -> Int) -> Int {
+        return op(args)
+    }
 
     func add(lhs: Int, rhs: Int) -> Int {
         return lhs + rhs
@@ -26,9 +30,17 @@ class Calculator {
         }
         return sum
     }
+    
+    func add(lhs: (Int, Int), rhs: (Int, Int)) -> (Int, Int) {
+        return (lhs.0 + rhs.0, lhs.1 + rhs.1)
+    }
 
     func subtract(lhs: Int, rhs: Int) -> Int {
         return lhs - rhs
+    }
+
+    func subtract(lhs: (Int, Int), rhs: (Int, Int)) -> (Int, Int) {
+        return (lhs.0 - rhs.0, lhs.1 - rhs.1)
     }
     
     func multiply(lhs: Int, rhs: Int) -> Int {
@@ -36,9 +48,13 @@ class Calculator {
     }
     
     func multiply(_ array: [Int]) -> Int {
-        var sum = 0
-        for number in array {
-            sum *= number
+        var sum = 1
+        if array.count != 0 {
+            for number in array {
+                sum *= number
+            }
+        } else {
+            return 0
         }
         return sum
     }
